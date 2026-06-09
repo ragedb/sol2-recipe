88e63cec2d5163342b1de1bb7a4a83db8adea91f
Max De Marzi <maxdemarzi@hotmail.com>
2026-06-09 01:56:30 +0000

    Migrate sol2 Conan recipe and test package to Conan 2.x

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # sol2-recipe
 Sol2 Conan Recipe for RageDB
 
-    conan create . 3.3.0-luajit@
-    conan create . 3.3.0-exhaustive@
-    conan create . 3.3.1-exhaustive@
+This recipe is compatible with Conan 2.x.
+
+## Usage
+
+To build, package, and test the library locally, run:
+
+```bash
+conan create . --version 3.3.0-luajit
+conan create . --version 3.3.0-exhaustive
+conan create . --version 3.3.1-exhaustive
+```
--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,8 @@
-from conans import ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.tools.scm import Version
+from conan.tools.files import get, copy
+from conan.tools.build import check_min_cppstd
+from conan.errors import ConanInvalidConfiguration
 import os
 
 
@@ -11,57 +14,48 @@ class Sol2Conan(ConanFile):
     topics = ("lua", "c++", "bindings")
     settings = "os", "arch", "compiler", "build_type"
     license = "MIT"
+    package_type = "header-library"
     no_copy_source = True
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
 
     @property
     def _compilers_minimum_version(self):
         return {
             "gcc": "7",
-            "Visual Studio": "15.7" if tools.Version(self.version) < "3.3.0" else "16",
+            "Visual Studio": "15.7" if Version(self.version) < "3.3.0" else "16",
+            "msvc": "191" if Version(self.version) < "3.3.0" else "192",
             "clang": "6",
             "apple-clang": "10",
         }
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, "17")
-
-        def lazy_lt_semver(v1, v2):
-            lv1 = [int(v) for v in v1.split(".")]
-            lv2 = [int(v) for v in v2.split(".")]
-            min_length = min(len(lv1), len(lv2))
-            return lv1[:min_length] < lv2[:min_length]
+            check_min_cppstd(self, "17")
 
         minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
         if not minimum_version:
-            self.output.warn("sol2 requires C++17. Your compiler is unknown. Assuming it supports C++17.")
-        elif lazy_lt_semver(str(self.settings.compiler.version), minimum_version):
-            raise ConanInvalidConfiguration("sol2 requires C++17 or higher support standard."
-                                            " {} {} is not supported."
-                                            .format(self.settings.compiler,
-                                                    self.settings.compiler.version))
-
+            self.output.warning("sol2 requires C++17. Your compiler is unknown. Assuming it supports C++17.")
+        elif Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"sol2 requires C++17 or higher support standard. "
+                f"{self.settings.compiler} {self.settings.compiler.version} is not supported."
+            )
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
         
     def requirements(self):
         self.requires("luajit/2.1.0-beta3-2023-01-04")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        extracted_dir = extracted_dir.replace("-luajit", "")
-        os.rename(extracted_dir, self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def package(self):
-        self.copy("LICENSE.txt", src=self._source_subfolder, dst="licenses")
-        self.copy("*.h", src=os.path.join(self._source_subfolder, "include"), dst="include")
-        self.copy("*.hpp", src=os.path.join(self._source_subfolder, "include"), dst="include")
+        copy(self, "LICENSE.txt", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, "*.h", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
+        copy(self, "*.hpp", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder, "include"))
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "sol2")
+        self.cpp_info.set_property("cmake_target_name", "sol2::sol2")
         self.cpp_info.defines.append("SOL_USING_CXX_LUA_JIT=1")
+        self.cpp_info.requires = ["luajit::luajit"]
